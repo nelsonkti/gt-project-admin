1e8015cecb12838d711d7490f2db84a22e6f9638
fuzengyao <571157865@qq.com>
2019-09-11 17:56:36 +0800

    初始化第三方文件

---
+++ vendor/catlane/chunk-file-upload/resources/views/chunk-file-upload.blade.php
@@ -0,0 +1,45 @@
+<div class="form-group {!! !$errors->has($label) ?: 'has-error' !!}" >
+
+    <label for="{{$id}}" class="col-sm-2 control-label" >{{$label}}</label>
+    <div class="col-sm-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+	    <span style="color: red;">
+            @include('admin::form.error')
+        </span>
+
+        <div id="wrapper">
+            <div id="container">
+                <!--头部，相册选择和格式选择-->
+
+                <div id="uploader">
+                    <div class="queueList">
+                        <div id="dndArea" class="placeholder">
+                            <div id="filePicker"></div>
+                            <p>或将文件拖到这里，单次只可选一张哦</p>
+                        </div>
+                    </div>
+                    <div class="statusBar" style="display:none;">
+                        <div class="progress">
+                            <span class="text">0%</span>
+                            <span class="percentage"></span>
+                        </div><div class="info"></div>
+                        <div class="btns">
+                            <div id="filePicker2"></div><div class="uploadBtn">开始上传</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="input-group">
+            <span class="input-group-addon"><i class="fa fa-pencil fa-fw"></i></span>
+            <input readonly style="background-color: #fff" type="text" id="{{$name}}-savedpath" name="{{$name}}" value="{{ old($column, $value) }}" class="form-control title" placeholder="{{$label}}">
+        </div>
+	    @include('admin::form.help-block')
+    </div>
+</div>
+
+
+
+
+
+
---
+++ vendor/encore/laravel-admin/src/Actions/Authorizable.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Encore\Admin\Actions;
+
+use Encore\Admin\Facades\Admin;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @mixin Action
+ */
+trait Authorizable
+{
+    /**
+     * @param Model $model
+     *
+     * @return bool
+     */
+    public function passesAuthorization($model = null)
+    {
+        if (!method_exists($this, 'authorize')) {
+            return true;
+        }
+
+        return $this->authorize(Admin::user(), $model) === true;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function failedAuthorization()
+    {
+        return $this->response()->error(__('admin.deny'))->send();
+    }
+}
